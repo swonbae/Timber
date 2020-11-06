0fd68693724def3fd04548253e63b87448a21119
swonbae <sbae13@my.centennialcollege.ca>
2020-11-06 00:35:26 -0500

    Added background image

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -15,6 +15,21 @@ int main()
 	// Create and open a window for the game
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
 
+	// Create a texture to hold a graphic on the GPU
+	Texture textureBackground;
+
+	// Load a graphic into the texture
+	textureBackground.loadFromFile("graphics/background.png");
+
+	// Create a sprite
+	Sprite spriteBackground;
+
+	// Attach the texture to the sprite
+	spriteBackground.setTexture(textureBackground);
+
+	// Set the spriteBackground to cover the screen
+	spriteBackground.setPosition(0, 0);
+
 	while (window.isOpen()) {
 		/***********************
 		Handle the players input
@@ -32,19 +47,9 @@ int main()
 		window.clear();
 
 		// Draw our game scene here
+		window.draw(spriteBackground);
 
 		// Show everything we just drew
 		window.display();
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
